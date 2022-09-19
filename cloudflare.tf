b0b9cf17179700bfa5c95d062393688e7e45cb7c
My Name <test-sl@i.ua>
2022-09-20 00:08:48 +0300

    cl

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,15 +1,23 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "cloudflare" {
-  email = "you@example.com"
-  token = "your-api-key"
+    api_token = {{secrets.CF_API_TOKEN}}
 }
 
 variable "domain" {
-  default = "example.com"
+  default = "megabank.net"
 }
 
 resource "cloudflare_record" "www" {
-  domain  = "${var.domain}"
-  name    = "www"
+  zone_id = "93be47cd9bf8560c9826cb1c012afbfa"
+  #domain  = "${var.domain}"
+  name    = "testci"
   value   = "203.0.113.10"
   type    = "A"
   proxied = true
