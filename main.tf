d81ef7cf71d7adc778041733fd95d9cbea597a05
My Name <test-sl@i.ua>
2022-09-20 23:19:16 +0300

    first

--- main.tf
+++ main.tf
@@ -1,68 +1,23 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  cloud {
+    organization = "sl-test"
+
+    workspaces {
+      name = "test-cloudflare"
+    }
+  }
 }
 
 provider "cloudflare" {}
-
-resource "aws_s3_bucket" "site" {
-  bucket = var.site_domain
-}
-
-resource "aws_s3_bucket_website_configuration" "site" {
-  bucket = aws_s3_bucket.site.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_bucket_acl" "site" {
-  bucket = aws_s3_bucket.site.id
-
-  acl = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "site" {
-  bucket = aws_s3_bucket.site.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource = [
-          aws_s3_bucket.site.arn,
-          "${aws_s3_bucket.site.arn}/*",
-        ]
-      },
-    ]
-  })
-}
-
-resource "aws_s3_bucket" "www" {
-  bucket = "www.${var.site_domain}"
-}
-
-resource "aws_s3_bucket_acl" "www" {
-  bucket = aws_s3_bucket.www.id
-
-  acl = "private"
-}
-
-resource "aws_s3_bucket_website_configuration" "www" {
-  bucket = aws_s3_bucket.www.id
-
-  redirect_all_requests_to {
-    host_name = var.site_domain
-  }
-}
 
 data "cloudflare_zones" "domain" {
   filter {
@@ -73,7 +28,7 @@ data "cloudflare_zones" "domain" {
 resource "cloudflare_record" "site_cname" {
   zone_id = data.cloudflare_zones.domain.zones[0].id
   name    = var.site_domain
-  value   = aws_s3_bucket_website_configuration.site.website_endpoint
+  value   = test.site.website_endpoint
   type    = "CNAME"
 
   ttl     = 1
