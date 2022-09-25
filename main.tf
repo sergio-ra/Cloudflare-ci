b371da696c63a6d7272d541481384e71faba3674
My Name <test-sl@i.ua>
2022-09-25 23:17:14 +0300

    4

--- main.tf
+++ main.tf
@@ -16,22 +16,30 @@ data "cloudflare_zones" "domain" {
   }
 }
 
-resource "cloudflare_record" "site_cname" {
-  zone_id = data.cloudflare_zones.domain.zones[0].id
-  name    = var.site_domain
-  value   = test.var.site_domain
-  type    = "CNAME"
-
-  ttl     = 1
-  proxied = true
+resource "cloudflare_record" "foobar" {
+  zone_id = var.cloudflare_zone_id
+  name    = "terraform"
+  value   = "192.168.0.11"
+  type    = "A"
+  ttl     = 3600
 }
 
-resource "cloudflare_record" "www" {
-  zone_id = data.cloudflare_zones.domain.zones[0].id
-  name    = "www"
-  value   = var.site_domain
-  type    = "CNAME"
+#resource "cloudflare_record" "site_cname" {
+#  zone_id = data.cloudflare_zones.domain.zones[0].id
+#  name    = var.site_domain
+#  value   = test.var.site_domain
+#  type    = "CNAME"
 
-  ttl     = 1
-  proxied = true
-}
+#  ttl     = 1
+#  proxied = true
+#}
+
+#resource "cloudflare_record" "www" {
+#  zone_id = data.cloudflare_zones.domain.zones[0].id
+#  name    = "www"
+#  value   = var.site_domain
+#  type    = "CNAME"
+
+#  ttl     = 1
+#  proxied = true
+#}
