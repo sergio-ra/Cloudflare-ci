495c9532f4b720255e31e784b1569842373553aa
sergio-ra <108478647+sergio-ra@users.noreply.github.com>
2022-10-02 23:19:00 +0300

    Update new.tf

--- new.tf
+++ new.tf
@@ -1,6 +1,6 @@
-resource "cloudflare_record" "testc3icd" {
-  zone_id = var.zone_id
-  name    = "testc3icd"
+resource "cloudflare_record" "testc3icd5" {
+  zone_id = var.CF_ZONE_ID_MGB_NET
+  name    = "testc3icd5"
   value   = "203.12.13.111"
   type    = "A"
   proxied = true
