ae2b14cdadd6ae10f11f148baac3873105047cde
sergio-ra <108478647+sergio-ra@users.noreply.github.com>
2022-10-02 23:21:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,14 +21,6 @@ provider "cloudflare" {
   api_token = var.CLOUDFLARE_API_TOKEN
 }
 
-#variable "zone_id" {
-#  default = "93be47cd9bf8560c9826cb1c012afbfa"
-#}
-
-#variable "domain" {
-#  default = "megabank.net"
-#}
-
 resource "cloudflare_record" "testcicd4" {
   zone_id = var.CF_ZONE_ID_MGB_NET
   name    = "testcicd4"
