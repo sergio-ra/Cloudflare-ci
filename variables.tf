b38c80a5a9ac5334b3589af49d620b9d1a9908c1
sergio-ra <108478647+sergio-ra@users.noreply.github.com>
2022-10-02 19:17:13 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,5 @@
-variable "aws_region" {
-  type        = string
-  description = "The AWS region to put the bucket into"
-  default     = "us-east-1"
-}
-
-variable "site_domain" {
-  type        = string
-  description = "The domain name to use for the static site"
-}
-
 variable "CF_API_TOKEN" {
   type        = string
   description = "Cloudflare API token"
   default     = ""
 }
-
-#variable "CF_API_TOKEN" {
-#  description = "Cloudflare API token"
-#  type = string
-#  default = ""
-#}
